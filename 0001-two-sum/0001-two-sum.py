a34ade808957d9a2f52b6c8d53fe9be5e4a7b016
Ranjith Kumar S <114894764+RanjithKumar100@users.noreply.github.com>
2026-04-24 15:30:19 +0530

    Time: 0 ms (100%), Space: 20.5 MB (44.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numToIndex = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if (nums[i]+nums[j]==target):
-                    return [i,j]
+            complement = target - nums[i]
+            if complement in numToIndex:
+                return numToIndex[complement], i
+            numToIndex[nums[i]] = i
